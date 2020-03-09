1a82932eb6222967c7722066cfea0594f51c21ac
aafmisra <aafmisra@gmail.com>
2020-03-09 16:48:15 -0400

    started testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import Enzyme, { shallow, render, mount } from 'enzyme';
+import Home from './components/Home'
+import Signup from './components/Signup';
+import AuthForm from './components/AuthForm';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('home component', () => {
+  // const user = {username: 'newuser', email: 'amisra218@gmail.com', password: 'hello123'}
+  // it('should render as expected', () => {
+  //   const component = shallow(<Home />)
+  //   expect(component.contains('Welcome')).toBe(true);
+  // })
+  it('renders children when passed in', () => {
+    const wrapper = render(
+      <Signup />
+    );
+    expect(wrapper.contains(<AuthForm />)).to.equal(true);
+  });
+
+})
+
