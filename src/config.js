0b83ec18f5194f198d3079e11898a2c79f920666
aafmisra <aafmisra@gmail.com>
2020-03-10 15:01:39 -0400

    changed http to https in config file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const APIURL =
     window.location.hostname === 'localhost'
         ? 'http://localhost:8000'
-        : 'http://what-cheer-api.herokuapp.com';
+        : 'https://what-cheer-api.herokuapp.com';
         
          
