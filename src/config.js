6f9577290fb3786a3a14476d04da6413f27403bd
aafmisra <aafmisra@gmail.com>
2020-03-11 23:54:13 -0400

    fixing config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,4 @@
 export const APIURL =
-         window.location.hostname === 'localhost'
-           ? 'https://what-cheer-api.herokuapp.com'
-           : 'http://localhost:8000';
-        
-         
+  window.location.hostname === 'localhost'
+    ? 'http://localhost:8000'
+    : 'https://what-cheer-api.herokuapp.com';
